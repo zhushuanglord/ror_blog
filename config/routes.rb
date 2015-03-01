a21ef27ae83a43ef620d8213e23dc57ac24831d1
root <root@ror.(none)>
2015-03-01 16:04:05 +0800

    add comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
-  resources :articles  
+  resources :articles do
+    resources :comments
+  end 
 
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
